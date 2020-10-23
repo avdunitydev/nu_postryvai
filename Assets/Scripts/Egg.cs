44821edeb9e422f9c7a9f654871146a0c019ac14
Andrii V <avd.g.dev@gmail.com>
2020-10-23 20:39:56 +0300

    Change logic Egg for animate.

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -4,46 +4,20 @@ using UnityEngine;
 
 public class Egg : MonoBehaviour
 {
-    public bool m_IsLeft;
-    public bool m_IsTop;
-
-    GameController m_Controller;
-
-    public void Init(bool isLeft, bool isTop, GameController controller)
-    {
-        m_Controller = controller;
-        m_IsLeft = isLeft;
-        m_IsTop = isTop;
-        gameObject.tag = "egg";
-
-    }
-
-
-
-    void EggCrashed()
-    {
-        m_Controller.m_hp.RemoveLife();
-        Destroy(gameObject);
-    }
-
     void OnCollisionExit2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "plane")
         {
-            Invoke("EggCrashed", 0.35f);
+            Invoke("EggCrash", .3f);
         }
 
     }
 
-    // Start is called before the first frame update
-    void Start()
+    void EggCrash()
     {
-
+        gameObject.SetActive(false);
+        transform.parent.GetComponent<EggController>().EggRemove();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
 }
---
+++ Assets/Scripts/EggController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggController : MonoBehaviour
+{
+    public GameObject m_EggGameObject;
+    public GameObject m_AnimDie;
+    public bool m_IsLeft { get; private set; }
+    public bool m_IsTop { get; private set; }
+
+    GameController m_Controller;
+
+    public void Init(bool isLeft, bool isTop, GameController controller)
+    {
+        m_Controller = controller;
+        m_IsLeft = isLeft;
+        m_IsTop = isTop;       
+    }
+
+    public void EggRemove()
+    {
+        m_Controller.m_hp.RemoveLife();
+        m_AnimDie.transform.parent.gameObject.SetActive(true);
+        if (m_IsLeft) m_AnimDie.GetComponent<Animator>().Play("dieLeft");
+        else m_AnimDie.GetComponent<Animator>().Play("dieRight");
+
+        Destroy(gameObject, 1.3f);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_EggGameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
