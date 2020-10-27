d076d5dd6a15362e02d34386ea51b2fcb3e06940
Andrii V <avd.g.dev@gmail.com>
2020-10-27 19:12:20 +0200

    Add one more enum.

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,9 +1,20 @@
 public static class GameData
 {
-    public static int HP = 1;
+    public static int HP = 0;
     public static int SCORE = 0;
-    public static float SPEED = 3f;
+    public static float SPEED = 0f;
+    public static float LINER_DRAG = 0f;
+    public static bool GAME_IS_RUN = false;
+
+    public enum enum_MusicMode
+    {
+        mute,
+        silentMode,
+        loudMode
+    }
 
     public static bool RandomTrueFalse() => (UnityEngine.Random.Range(-1.0f, 1.0f) >= 0) ? true : false;
+    public static bool IsMobile() => (UnityEngine.Application.isMobilePlatform) ? true : false;
+
 
 }
