6e77a6ef66414012b15397117229af30af1b7c80
Andrii V <avd.g.dev@gmail.com>
2020-10-28 18:56:11 +0200

    Add Bell when Game Speed Up.

---
+++ Assets/Scripts/Bell.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Bell : MonoBehaviour
+{
+    GameController gameController;
+
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        gameController.OnSpeedUp += UpLevel;
+    }
+
+    private void UpLevel()
+    {
+        GetComponent<Animator>().Play("bell");
+        GetComponent<AudioSource>().Play();
+    }
+
+}
